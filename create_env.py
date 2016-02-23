f52d9e58f2ca94a0e095f6a47ae7f8bad22a1cbe
Helorem <helorem@localhost>
2016-02-23 14:23:46 +0100

    Working based on templates

---
+++ create_env.py
@@ -0,0 +1,71 @@
+import os
+
+def normalize_name(name):
+    return name.replace(r'([A-Z])', "_\1").replace(" ", "_").lower()
+
+def adapt_template(data, target_root_path, path, filename, target_filename = None):
+    project_name = normalize_name(data["APP_NAME"])
+    if target_filename is None:
+        target_filename = filename
+    else:
+        target_filename = target_filename.replace("@APPNAME@", project_name)
+
+    output_str = ""
+    with open(os.path.join("templates", path, filename), "r") as fd:
+        output_str = fd.read()
+    output_str = output_str.replace("@APPNAME@", project_name)
+    output_str = output_str.replace("@VERSION@", data["APP_VERSION"])
+    output_str = output_str.replace("@MAINTENER@", data["APP_MAINTENER"])
+    output_str = output_str.replace("@DESCRIPTION@", data["APP_DESCRIPTION"])
+
+    target_path = os.path.join(target_root_path, path)
+
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+    with open(os.path.join(target_path, filename), "w") as fd:
+        fd.write(output_str + "\n")
+
+if __name__ == "__main__":
+    spec_data = {
+            "APP_NAME" : {"value" : "", "description" : "Project name", "mandatory" : True},
+            "APP_VERSION" : {"value" : "1.0.0", "description" : "Project version", "mandatory" : False},
+            "APP_MAINTENER" : {"value" : "Romain CONNESSON", "description" : "Maintener", "mandatory" : False},
+            "APP_DESCRIPTION" : {"value" : "", "description" : "Description", "mandatory" : False},
+        }
+    data_order = ["APP_NAME", "APP_VERSION", "APP_MAINTENER", "APP_DESCRIPTION"]
+
+    data= {}
+    for key in data_order:
+        item = spec_data[key]
+        loop = True
+        while loop:
+            txt = item["description"]
+            if not item["mandatory"]:
+                txt += ' (Default : "%s")' % item["value"]
+            res = raw_input("%s : " % txt)
+            if res:
+                data[key] = res
+                loop = False
+            if item["mandatory"] and not res:
+                print "This parameter is mandatory"
+            else:
+                data[key] = item["value"]
+                loop = False
+
+    files = [
+        ("build/DEBIAN", "control", None),
+        ("build/DEBIAN", "postinst", None),
+        ("build/DEBIAN", "prerm", None),
+        ("build", "Dockerfile.dev", None),
+        ("build", "Dockerfile.deploy", None),
+        ("build", "install_dev.sh", None),
+        ("build", "install_nginx.sh", None),
+        ("conf", "nginx_default.conf", None),
+        ("conf", "srv.service", "@APPNAME@.service"),
+        (".", "Makefile", None),
+        (".", "README.md", None),
+    ]
+
+    target_root_path = "bin"
+    for path, filename, target_filename in files:
+        adapt_template(data, target_root_path, path, filename, target_filename)
---
+++ templates/build/install_dev.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm -f /etc/init.d/@APPNAME@
+ln -s /mnt/src/conf/@APPNAME@.service /etc/init.d/@APPNAME@
+
+rm -f /usr/bin/@APPNAME@.sh
+ln -s /mnt/src/src/api/@APPNAME@.sh /usr/bin/@APPNAME@.sh
+
+rm -rf /var/www/@APPNAME@
+ln -s /mnt/src/src /var/www/@APPNAME@
+
+rm -f /etc/nginx/sites-available/@APPNAME@
+ln -s /mnt/src/conf/nginx_default.conf /etc/nginx/sites-available/@APPNAME@
+ln -s /etc/nginx/sites-available/@APPNAME@ /etc/nginx/sites-enabled/@APPNAME@
+
+sqlite3 /var/www/@APPNAME@/api/@APPNAME@.db < /var/www/@APPNAME@/api/@APPNAME@.sql
+chown -R www-data:www-data /var/www/@APPNAME@/api
+
