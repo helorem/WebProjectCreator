5014a1c86a2c4c08f642b8ede7b0533cfcc004b6
Helorem <helorem@localhost>
2016-03-04 17:55:05 +0100

    Separate nginx conf on host + systemd service + systemd in docker + api file rights

--- templates/build/install_dev.sh
+++ templates/build/install_dev.sh
@@ -13,6 +13,10 @@ rm -f /etc/nginx/sites-available/@APPNAME@
 ln -s /mnt/src/conf/nginx_default.conf /etc/nginx/sites-available/@APPNAME@
 ln -s /etc/nginx/sites-available/@APPNAME@ /etc/nginx/sites-enabled/@APPNAME@
 
+rm -f /var/www/@APPNAME@/api/@APPNAME@.db
 sqlite3 /var/www/@APPNAME@/api/@APPNAME@.db < /var/www/@APPNAME@/api/@APPNAME@.sql
 chown -R www-data:www-data /var/www/@APPNAME@/api/@APPNAME@.db
-
+touch /var/log/@APPNAME@.log
+chown www-data:www-data /var/log/@APPNAME@.log
+mv /var/log/@APPNAME@.log /var/www/@APPNAME@/api/@APPNAME@.log
+ln -s /var/www/@APPNAME@/api/@APPNAME@.log /var/log/@APPNAME@.log
--- templates/build/install_nginx.sh
+++ templates/build/install_nginx.sh
@@ -8,17 +8,17 @@ mkdir -p bin
 ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' @APPNAME@)"
 cp build/nginx_host.conf bin/nginx_host.conf
 sed -i "s/@IP@/$ip/g" bin/nginx_host.conf
-sed -i "s/$/ $PATTERN/" bin/nginx_host.conf
 
 # Remore old addon
 cp /etc/nginx/sites-available/default bin/default
 sed -i "/ $PATTERN/d" bin/default
+sudo rm /etc/nginx/conf.d/moneoresto-checker
 
 # Apply
-sed -i 's#location / {#location / {\n@BLK@#g' bin/default
-sed -i -e '/@BLK@/ {' -e 'r bin/nginx_host.conf' -e 'd' -e '}' bin/default
+sed -i "s@location / {@location / {\ninclude conf.d/@APPNAME@; $PATTERN@g" bin/default
 
 # Move
+sudo mv bin/nginx_host.conf /etc/nginx/conf.d/moneoresto-checker
 sudo mv bin/default /etc/nginx/sites-available/default
 
 # Restart nginx
